9dfb589dde47401f7aa90528cf61cf65afa03ad4
Mark Olbert <mark@arcabama.com>
2020-05-06 09:52:21 -0700

    Deleting deprecated projects

--- J4JSmsLogging/IJ4JSms.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace J4JSoftware.Logging
-{
-    public interface IJ4JSms
-    {
-        void Send( string mesg );
-    }
-}
--- J4JSmsLogging/IJ4JSmsLoggerConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.IO;
-
-namespace J4JSoftware.Logging
-{
-    public interface IJ4JSmsLoggerConfiguration : IJ4JLoggerConfiguration
-    {
-        StringWriter SmsWriter { get; }
-    }
-}
