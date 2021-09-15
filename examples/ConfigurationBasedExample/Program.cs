4072c5bab9ec9d47a02b2646d91f60b3e78e3eb8
Mark Olbert <mark@arcabama.com>
2021-09-14 18:35:35 -0700

    Added configuration-based example

---
+++ examples/ConfigurationBasedExample/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using J4JSoftware.Logging;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace ConfigurationBasedExample
+{
+    // shows how to use J4JLogger with a configuration file
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loggerConfig = new J4JLoggerConfiguration()
+                {
+                    CallingContextToText = ConvertCallingContextToText
+                }
+                .AddEnricher<CallingContextEnricher>();
+
+            var configRoot = new ConfigurationBuilder()
+                .AddJsonFile( Path.Combine( Environment.CurrentDirectory, "appConfig.json" ), true )
+                .Build();
+
+            loggerConfig.SerilogConfiguration
+                .ReadFrom
+                .Configuration( configRoot );
+                
+            var logger = loggerConfig.CreateLogger();
+            logger.SetLoggedType(typeof(Program));
+
+            logger.Information("This is an Informational logging message");
+            logger.Fatal("This is a Fatal logging message");
+        }
+
+        private static string ConvertCallingContextToText(
+            Type? loggedType,
+            string callerName,
+            int lineNum,
+            string srcFilePath)
+        {
+            return CallingContextEnricher.DefaultConvertToText(loggedType,
+                callerName,
+                lineNum,
+                CallingContextEnricher.RemoveProjectPath(srcFilePath, GetProjectPath()));
+        }
+
+        private static string GetProjectPath([CallerFilePath] string filePath = "")
+        {
+            var dirInfo = new DirectoryInfo(Path.GetDirectoryName(filePath)!);
+
+            while (dirInfo.Parent != null)
+            {
+                if (dirInfo.EnumerateFiles("*.csproj").Any())
+                    break;
+
+                dirInfo = dirInfo.Parent;
+            }
+
+            return dirInfo.FullName;
+        }
+    }
+}
